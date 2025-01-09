2b56b8b19d61c1ab805cc86fab9d7e155c54d6ec
oxfoxlion <oxfoxlion@gmail.com>
2025-01-09 17:05:44 +0800

    更新路徑

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/ReactPracticeProject2/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/ReactWeek2_0109/' : '/',
   plugins: [react()],
 })
