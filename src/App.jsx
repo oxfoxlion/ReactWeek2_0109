318f93960cace29cd3a107b9e6b2e02d281e110d
oxfoxlion <oxfoxlion@gmail.com>
2025-01-09 18:19:08 +0800

    完成取得產品資料

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,98 @@
+import axios from 'axios'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+const baseUrl = import.meta.env.VITE_BASE_URL;
+const apiPath = import.meta.env.VITE_API_PATH;
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isSignIn, setIsSignIn] = useState(false);
+  const [products,setProducts] = useState([]);
+
+
+    const signIn = async (e)=>{
+      e.preventDefault();
+
+        try {
+          const response = await axios.post(`${baseUrl}/admin/signin/`, {
+            username: email,
+            password: password
+          })
+  
+          const {token,expired} = response.data;
+          console.log(token,expired);
+          document.cookie = `shaoToken=${token}; expires=${new Date(expired)}`;
+          axios.defaults.headers.common['Authorization'] = token;
+
+          alert('登入成功');
+          setIsSignIn(true);
+        }
+        catch {
+          alert('登入失敗');
+          setEmail("");
+          setPassword("");
+        }
+
+    }
+
+    const checkIsSignIn = async()=>{
+      try{
+        await axios.post(`${baseUrl}/api/user/check`)
+        console.log('驗證成功');
+      }
+      catch{
+        console.log('驗證失敗，即將回到登入頁面');
+        setIsSignIn(false); 
+      }
+    }
+    
+    const getProducts = async()=>{
+      try{
+        const response = await axios.get(`${baseUrl}/api/${apiPath}/admin/products`)
+        console.log(response.data);
+      }
+      catch{
+        console.log('資料取得失敗')
+      }
+    }
+    
+
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {isSignIn ? (
+        <div className='container my-5'>
+          <button type='button' className='btn btn-secondary' onClick={()=>checkIsSignIn()}>驗證使用者是否登入</button>
+          <button type='button' className='btn btn-secondary' onClick={()=>getProducts()}>取得產品資料</button>
+          <div className='row'>
+            <div className="col-6">
+              <table>
+
+              </table>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className='container my-5'>
+          <h2 className='text-center'>請先登入</h2>
+          <form className='text-center w-25 mx-auto p-3'>
+            <div className="my-2">
+            <label htmlFor="email" className='form-label'>Email</label>
+            <input value={email} type='email' id="email" className='form-control' onChange={(e) => setEmail(e.target.value)}></input>
+            </div>
+            <div className="my-2">
+            <label htmlFor="password" className='form-label'>Password</label>
+            <input value={password} type='password' id='password' className='form-control' onChange={(e) => setPassword(e.target.value)}></input>
+            </div>
+            <button className='btn btn-primary' onClick={signIn}>登入</button>
+          </form>
+        </div>
+      )}
+
+
     </>
   )
 }
